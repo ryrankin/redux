696f00146ef9c012aa1737b98676a6ab56231a8b
Ryan Rankin <ryrankin@gmail.com>
2017-04-18 10:23:58 -0400

    thunk added

--- js/store.js
+++ js/store.js
@@ -1,7 +1,8 @@
 import {createStore} from 'redux';
 import * as actions from './actions/index';
+import thunk from 'redux-thunk';
 import store from './store';
 import * as reducers from './reducers/index';
 
 
-export default createStore(reducers.repositoryReducer);
+export default createStore(reducers.repositoryReducer, applyMiddleware(thunk));
